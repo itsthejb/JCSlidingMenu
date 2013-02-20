0e0220c258ac334072413b8053fc5d586565334e
Jon Crooke <joncrooke@gmail.com>
2013-02-20 15:27:18 +0100

    Initial Commit

---
+++ SlidingMenu/JCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCAppDelegate.h
+//  SlidingMenu
+//
+//  Created by Jon Crooke on 20/02/2013.
+//  Copyright (c) 2013 jc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlidingMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SlidingMenu
+//
+//  Created by Jon Crooke on 20/02/2013.
+//  Copyright (c) 2013 jc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCAppDelegate class]));
+  }
+}
